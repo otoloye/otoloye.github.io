2dc72789c8b9a4438586563a1a36a980ba47cd24
Otoloye Oyeniran <otoloyeoyeniran@gmail.com>
2017-08-11 21:32:42 +0100

    Adds coming soon template

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(window).load(function() {
+	
+	  setTimeout(function() {
+		$('.spinner').fadeOut("slow");
+			
+		  setTimeout(function() {
+		  $('#prelaoder').fadeOut("slow"); 
+		  
+			  setTimeout(function() {
+				  $('.content-block').addClass('animated fadeInDown').fadeIn("slow");
+			       $('#footer').fadeIn('slow');
+				   
+			  }, 900);
+		  }, 700);	  
+	  }, 700);	
+  
+});
